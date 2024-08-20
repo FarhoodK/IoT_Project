c67f7a3504fcdc7a6224a428b7e8c73f106a637b
lapodalessandris <122673987+lapodalessandris@users.noreply.github.com>
2024-08-20 12:36:43 +0200

    Update and rename main to main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from smartender import Smartender
+
+if __name__ == "__main__":
+    smartender = Smartender('cocktails.json')
+    smartender.show_cocktails(smartender.available_cocktails)
+    smartender.configure()
+
+    action = input("Press R to run, T to test\t")
+    if action.lower() == "r":
+
+        while True:
+            smartender.show_cocktails(smartender.selected_cocktails)
+            smartender.make_cocktail()
+
+    else:
+        if smartender.selected_cocktails:
+            cocktail_name = smartender.selected_cocktails[0].name
+
+        for _ in range(20):
+            print(f"\nPreparing {cocktail_name} ({_+1}/10)...")
+            smartender.make_cocktail(cocktail_name)
+
+print("daje")
