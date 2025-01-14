09713a3ff7577959520fea47e6791d82120a691a
FarhoodK <farhood_kankash@live.com>
2025-01-14 15:19:05 +0100

    The REST and API

--- main_API.py
+++ main_API.py
@@ -1,1 +1,12 @@
 "Update the logic of the main.py file here in order to make it run with the RESTful Smartender class in the smartender_REST.py file"
+import cherrypy
+from smartender_REST import SmartenderREST
+
+if __name__ == "__main__":
+    cherrypy.config.update({
+        'server.socket_host': '0.0.0.0',
+        'server.socket_port': 8080,
+        'log.screen': True
+    })
+
+    cherrypy.quickstart(SmartenderREST(), "/api", {"/": {"tools.json_out.on": True}})
